db6e61b150f573e68677973ab4ab433879c7d209
machina20 <ryanschwa2@gmail.com>
2023-07-23 14:41:35 -0700

    added first page text (not formatted to fit picture)

---
+++ src/app/navbar.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+export default async function Navbar() {
+  return (
+    <nav className="flex text-[#707070] justify-between w-full text-xs">
+      <LinkItem name="RYAN SCHWARTZ"></LinkItem>
+
+      <div className="space-x-4">
+        <LinkItem name="About"></LinkItem>
+        <LinkItem name="Projects"></LinkItem>
+        <LinkItem name="Contact"></LinkItem>
+      </div>
+    </nav>
+  );
+}
+
+interface LinkItemProps {
+  name: string;
+  link?: string;
+}
+function LinkItem({ name, link }: LinkItemProps) {
+  return (
+    <a href={link} className="transition-colors hover:text-white">
+      {name}
+    </a>
+  );
+}
