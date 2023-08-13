2c31ca8222de985c8587d79d236b3ccffba997af
machina20 <ryanschwa2@gmail.com>
2023-08-13 08:01:31 -0700

    removed export from the authOptions declaration

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,7 @@ import  CredentialsProvider  from "next-auth/providers/credentials";
 import { prisma } from "../../../../lib/prisma";
 import { compare } from "bcrypt";
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   pages:{
     signIn: '/login'
   },
